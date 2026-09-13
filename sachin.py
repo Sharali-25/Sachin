cf7fbfd66e7132359cddca1aae080c49aca7f9e0
Sharali-25 <Sharaliarkansas@gmail.com>
2026-09-13 11:53:31 -0500

    Add files via upload

---
+++ sachin.py
@@ -0,0 +1,9 @@
+print('Hi Sharali is fool')
+Height = 6
+Weight = 200
+print("Sharali Height in Feet",Height)
+print("Sharali Weight in Pounds", Weight)
+n = int(input("Enter your name : "))
+print("Hi ",n,"Nice to meet you" )
+list = [40, 400, 500, 600]
+print(list)
